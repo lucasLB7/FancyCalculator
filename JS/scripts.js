07f5356cf3b79061a1178fecfcd4bc1f2f97d33c
Luka <plucaslambert@gmail.com>
2018-03-22 16:49:16 +0300

    First part of the calculator is done..

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,17 +1,36 @@
-var add = function(number1, number2) {
+
+      var add = function(number1, number2) {
         return number1 + number2;
       };
-var substract = function(number1, number2) {
+
+      var substract = function(number1, number2) {
         return number1 - number2;
       };
-var multiply = function(number1, number2) {
+
+      var multiply = function(number1, number2) {
         return number1 * number2;
       };
-var divide = function(number1, number2) {
-       return number1 / number2;
+
+      var divide = function(number1, number2) {
+        return number1 / number2;
       };
 
+      $(document).ready(function() {
+        $("form#add").submit(function(event) {
+          event.preventDefault();
+          var number1 = parseInt($("#add1").val());
+          var number2 = parseInt($("#add2").val());
+          var result = add(number1, number2);
+          $("#output").text(result);
+        });
+          });
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-alert(add(number1, number2));
+        $(document).ready(function() {
+          $("form#substract").submit(function(event) {
+            event.preventDefault();
+            var number1 = parseInt($("#subb1").val());
+            var number2 = parseInt($("#subb2").val());
+            var result = substract(number1, number2);
+            $("#output").text(result);
+          });
+            });
